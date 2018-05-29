3483871f579567135e862a45356466783f2f43dc
libicc <libicc@126.com>
2018-05-29 16:32:19 +0800

    commit path and a raw script. add some explaination for the data index and data structure

---
+++ path.py
@@ -0,0 +1,5 @@
+import os
+
+path = 'E:/BaiduNetdiskDownload/antfin/raw_data'
+train =  os.path.join(path, 'atec_anti_fraud_train.csv') 
+test = os.path.join(path, 'atec_anti_fraud_test_a.csv')
---
+++ script.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+from path import *
+
+train_data = pd.read_csv(train, index_col=1)
+test_data = pd.read_csv(test, index_col=1)
+
+
+
